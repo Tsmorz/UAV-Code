ea09baebbc5526fb2f29fcc74ada74d61bce9a92
Tony Smoragiewicz <tonysmoragiewicz@gmail.com>
2021-08-20 14:54:37 +0200

    Added fuction to pull inputs from a CSV file

---
+++ ReadInputs.py
@@ -0,0 +1,12 @@
+from csv import reader
+import numpy as np
+
+def ReadInputs(__file__):
+    constraints = []
+    with open(__file__, newline='') as csvfile:
+        filereader = reader(csvfile, delimiter=',', quotechar='|')
+        for row in filereader:
+            constraints = np.hstack([constraints,row[1]])
+    constraints = constraints[1:]
+    constraints = constraints.astype(float)
+    return constraints
